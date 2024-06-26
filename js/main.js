f0c3c93d64312b6c5f839e1dfc380f34e751edda
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-06-26 09:06:39 -0500

    feat: :construction: avances en la estructuracion con la data
    se siguio obteniendo la data de los cohetes y se pone en plantillas especificas

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import { getAllRockets } from "./components/rockets.js";
-import { nameRockets } from "./modules/gallery.js";
+import { nameRockets, infoRockets } from "./modules/gallery.js";
 
 let header__title = document.querySelector(".header__title")
+let description__item = document.querySelector(".description__item")
 
 addEventListener("DOMContentLoaded", async()=>{
     let info = await getAllRockets()
@@ -9,4 +10,6 @@ addEventListener("DOMContentLoaded", async()=>{
     console.log(info);
 
     header__title.innerHTML = await nameRockets(info)
+
+    description__item.innerHTML = await infoRockets(info)
 })
--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -13,10 +13,13 @@ export const infoRockets = (info)=>{
     let plantilla = ""
 
     plantilla+= /*html*/`
-    <div class="load"></div>
-    <div class="load"></div>
-    <div class="load"></div>
-    <div class="load"></div>
-    <div class="load"></div>
-    `
+    <div id="description__item" class="description__item">
+        <div class="load">${info.country}</div>
+        <div class="load">${info.description}</div>
+        <div class="load">The estimated cost per rocket launch ${info.cost_per_launch}</div>
+        <div class="load">The date of the first flight of the rocket ${info.first_flight}</div>
+        <div class="load">Read more about the rocket ${info.wikipedia}</div>
+    </div>
+    `   
+    return plantilla
 }
