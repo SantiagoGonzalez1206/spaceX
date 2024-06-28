8fc025476ea77428adbc761e935d31b9fd0d1769
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-06-28 17:05:29 -0500

    feat: :sparkles: integracion de informacion en centro main
    se realizaron tablas y stats con la importacion de la data

--- js/components/pagination.js
+++ js/components/pagination.js
@@ -1,5 +1,5 @@
 import { getRocketMoreInfoById, getRocketNameById, idRocket} from "../modules/info.js";
-import { nameRocket, infoRocket, moreInfoRocket} from "../modules/gallery.js";
+import { nameRocket, infoRocket, moreInfoRocket, lastInfoRocket} from "../modules/gallery.js";
 
 
 export const paginationRockets = async () => {
@@ -21,6 +21,7 @@ export const paginationRockets = async () => {
         await nameRocket(rocketName);
         await infoRocket(rocketMoreInfo);
         await moreInfoRocket(rocketMoreInfo)
+        await lastInfoRocket(rocketMoreInfo)
 
     }
 
@@ -38,6 +39,7 @@ export const setupPagination = async () => {
         await nameRocket(rocketName);
         await infoRocket(rocketMoreInfo);
         await moreInfoRocket(rocketMoreInfo)
+        await lastInfoRocket(rocketMoreInfo)
     
         
     });
